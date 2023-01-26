aa790d5c9fbbf34976a3ac42470e2b1199c5a56d
AFAD85 <antondiepenhorst@gmail.com>
2023-01-26 15:02:21 +0100

    started character creation module/ fight logic (in notes)

---
+++ character_creation.py
@@ -0,0 +1,26 @@
+from typing import Protocol
+
+
+class Character(Protocol):
+    
+    def attack_adversary(self):
+        ...
+        
+
+class PlayerCharacterSelection():
+    
+    def __init__(self):
+        self.character_1 = None
+        self.character_2 = None
+        self.character_3 = None
+
+    
+    
+    
+    
+    
+class ComputerCharacterSelection():
+    
+    
+    
+    
