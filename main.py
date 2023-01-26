b6ceb7a78029921d351ddd21645451e584065d4f
AFAD85 <antondiepenhorst@gmail.com>
2023-01-26 13:55:02 +0100

    Wrote initial design, started main function

---
+++ main.py
@@ -0,0 +1,23 @@
+def main():
+    
+    print(f"-------------------------------------\n",
+          "Welcome to Pokemon fighting!\n",
+          "Main Menu\n",
+          "1. Start Game",
+          "")
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
