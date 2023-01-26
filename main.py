dbc293be9ea707428659833b0899baa26ff487c6
AFAD85 <antondiepenhorst@gmail.com>
2023-01-26 14:10:06 +0100

    main menu

--- main.py
+++ main.py
@@ -1,10 +1,40 @@
 def main():
     
-    print(f"-------------------------------------\n",
-          "Welcome to Pokemon fighting!\n",
-          "Main Menu\n",
-          "1. Start Game",
-          "")
+    
+    # displays main menu
+
+    
+    while True:
+        print(f"-------------------------------------\n",
+        "Welcome to Pokemon fighting!\n",
+        "Main Menu\n",
+        "1. Start Game\n",
+        "2. Select fighters\n",
+        "3. Exit\n"
+        )
+        
+        
+        # asks user for input
+        selection = get_user_input_main_menu()
+        
+        # checks user input and proceeds with corresponding function
+        if selection == "1":
+            pass
+        
+        if selection == "2":
+            pass
+
+        if selection == "3":
+            break
+
+        else:
+            print("Invalid input, please select 1, 2 or 3 and press enter")
+
+
+def get_user_input_main_menu():
+    selection = input("type 1, 2 or 3 and press enter - "
+                    )
+    return selection
     
     
     
