2d6d41b58232421c7acaa9ad9a504d7d266fdac9
AFAD85 <antondiepenhorst@gmail.com>
2023-01-26 15:20:06 +0100

    fight for 1 turn function

--- game.py
+++ game.py
@@ -2,6 +2,8 @@
 
 class GameLoop():
     
+    while True:
+        
 
 class PerformAttack():
     
--- pokemon.py
+++ pokemon.py
@@ -5,7 +5,11 @@ class PokemonFighter():
     def __init__(self):
         self.health = 0
         self.attack_power = 0
-
+        self.speed = 0
+        
+    def attack(self, computer):
+        attack = self.attack_power - computer.attack_power
+        return attack
 
     
     
