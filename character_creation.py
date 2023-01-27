47fc6bf066193c44d4e23d3aacd7e9691f54fc8a
AFAD85 <antondiepenhorst@gmail.com>
2023-01-27 14:26:46 +0100

    character creation definined Fighter class, working on pokemon

--- character_creation.py
+++ character_creation.py
@@ -1,18 +1,29 @@
 from abc import ABC, abstractmethod
 
 
+
 class Fighter(ABC):
+    
+    def __init__(self, name: str, health: int, controler: str):
+        self.name = name
+        self.health = health
+        self.controler = controler
+    
     @abstractmethod
     def attack() -> None:
         pass
+    
+    @abstractmethod
+    def display_stats() -> None:
 
 
 
 class Pokemon(Fighter):
     
-    def __init__(self, name, controler):
+    def __init__(self, name, health, controler):
+        super().__init__(name, health)
         self.name = name
-        self.health = 10
+        self.health = health
         self.attack_power = 2
         self.knocked_out = False
         self.controler = controler
@@ -26,6 +37,27 @@ class Pokemon(Fighter):
         """
         self.attack_power = attack_power
         
+    def display_stats():
+        
+        
+class Charmander(Pokemon):
+    
+    def __init__(self, name, controler)
+
+
+class PlayerCharacterSelection:
+    
+    def __init__(self):
+        self.character_1 = None
+        self.character_2 = None
+        self.character_3 = None
+
+    
+
+# class ComputerCharacterSelection:
+    
+    
+
 
 
 
