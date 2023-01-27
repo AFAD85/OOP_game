3a53257b60bf47b602f5a2d29339014621012d42
AFAD85 <antondiepenhorst@gmail.com>
2023-01-27 17:31:22 +0100

    cleaned main

--- main.py
+++ main.py
@@ -3,66 +3,19 @@ from game import GameLoop
 from menus import MainMenu, CharacterSelectionMenu
 
 
-
+    
+    
 def main():
-    
-    
-    # displays main menu
-
-    
-    while True:
-
-        menu = MainMenu()
-        menu.display_menu()
-        
-        
-        # asks user for input
-        selection = get_user_input_main_menu()
-        
-        # checks user input and proceeds with corresponding function
-        if selection == "1":
-            print("starting game!")
-            
-            # game loop initilized here
-            player = None# object made in character creation
-            computer = None# object made in character creation
-            if not player or computer:
-                print("No characters selected, please use the character creator before starting the game.")
-                main()
-            game = GameLoop(player, computer)
-            game.execute_fight_turn()
-
-        
-        if selection == "2":
-            print("Select fighters")
-            # selection menu initialized here
-            break
-
-        if selection == "0":
-            print("exiting game")
-            break
-
-        else:
-            print("Invalid input, please select 1, 2 or 0 and press enter")
-
-
-def get_user_input_main_menu():
-    selection = input("type 1, 2 or 0 and press enter - "
-                    )
-    return selection
-    
-    
-    
-    
-    
-
-
-
-
-
-
-
+    menu = MainMenu()
+    menu.run()
 
 if __name__ == "__main__":
     main()
+    
+    
+
+
+
+
+
 
