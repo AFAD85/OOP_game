52ac1f0bdd730957bde9b262dc42f00314dcd406
AFAD85 <antondiepenhorst@gmail.com>
2023-01-27 16:55:38 +0100

    menus file to eliviate main function (was getting crowded)

---
+++ menus.py
@@ -0,0 +1,39 @@
+"""
+menu file
+
+"""
+
+class MainMenu():
+    
+    def __init__(self) -> None:
+        pass
+    
+    def display_menu(self):
+        print("""
+              Welcome to Pokemon fighting!
+              Main Menu
+              1. Start Game
+              2. Select fighters
+              0. Exit
+              """)
+    
+    
+    
+    
+    
+    
+    
+class CharacterSelectionMenu():
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
